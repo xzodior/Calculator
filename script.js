7cae3d9140be5898aed3822a1eb24600a82481f5
xzodior <sadiqur161001@gmail.com>
2022-05-14 16:38:37 +0100

    Created 3 functions vital to this project. One was chooseOperation() which is to ensure the calculator knows which operand to use when calculating and also to allow the user to see the previous operand so they can see what they typed. The compute function which is somewhat the same as operate() function except it doesn't take 2 parameters. Compute() function works better this way and doesn't require as many methods as the other. Finally, the displayUpdate() function were it displays event occurences like the buttons presssed as well as the operation.

--- script.js
+++ script.js
@@ -51,12 +51,54 @@ class Calculator {
     }
 
     applyNumber(number) {
-        this.currentOperand = this.currentOperand.toString() + number.toString();
+        this.currentOperand = this.currentOperand.toString() + number.toString();       // convert to string so we can add the number to the end of a string. toString() is used as JS would add these as numbers appended instead of added like 2+2=22.
+    }
+
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return 
+        if (this.previousOperand !== '') {
+            this.compute()
+        }
+        this.operation = operation                          // so the calculator knows which operand to use when calculating
+        this.previousOperand = this.currentOperand;         
+        this.currentOperand = '';                           // Allows the user to type out another operand whilst displaying the previous operand
+    }
+
+    compute(){
+        let computation;
+        const prev = parseInt(this.previousOperand)
+        const current = parseInt(this.currentOperand)
+        if(isNaN(prev) || isNaN(current)) return 
+        switch(this.operation){
+            case '+':
+                computation = prev + current
+                break;
+            case '-':
+                computation = prev - current
+                break;
+            case 'x':
+                computation = prev * current
+                break;
+            case '/':
+                computation = prev / current
+                break;
+            default:
+                return;
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
     }
 
     displayUpdate() {
-        this.currentOperand.innerText = this.currentOperand;
+        currentOperand.innerText = this.currentOperand;         // So the buttons pressed will be displayed on the calculator
+        if (this.operation != null){
+            previousOperand.innerText = `${this.previousOperand} ${this.operation}`;    // displays previous operand with the operation so the user knows what arithmetic expression they clicked      
+        } else {
+            previousOperand.innerText = ''
+        }
     }
+
 }
 
 const numberButtons = document.querySelectorAll('[data-number]');
@@ -66,11 +108,28 @@ const allClear = document.querySelector('[data-clear]');
 const currentOperand = document.querySelector('[data-current]');
 const previousOperand = document.querySelector('[data-previous]');
 
-const calculator = new Calculator (previousOperand, currentOperand);
+const calculator = new Calculator (previousOperand, currentOperand);        // pass the elements to the class constructor
+
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-        calculator.applyNumber(button.innerText);
+        calculator.applyNumber(button.innerText);               // stores numbers pressed into the applyNumber function
+        calculator.displayUpdate();                             
+    })
+})
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
         calculator.displayUpdate();
     })
 })
 
+equal.addEventListener('click', button => {
+    calculator.compute()
+    calculator.displayUpdate();
+})
+
+allClear.addEventListener('click', button => {
+    calculator.clear()
+    calculator.displayUpdate();
+})
